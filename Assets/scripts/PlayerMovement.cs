1b1e7f91dc2506976933ba9989cc28eca0a4a6d4
thedoodler <ramprakashsmail@gmail.com>
2020-12-06 20:00:29 +0530

    first log

--- Assets/scripts/PlayerMovement.cs
+++ Assets/scripts/PlayerMovement.cs
@@ -5,7 +5,7 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        Debug.Log("hello");
     }
 
     // Update is called once per frame
